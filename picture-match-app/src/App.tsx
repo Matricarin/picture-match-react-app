f47dba943b771c6b803db3165cb1534be9f9a101
matricarin <matricarin@gmail.com>
2026-08-13 15:39:56 +0500

    render components

--- picture-match-app/src/App.tsx
+++ picture-match-app/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from "./components/Card";
 
 function App() {
   const title = "Picture Match!";
@@ -8,7 +9,15 @@ function App() {
   return (
     <div>
       <h1>{title}</h1>
-      <p>Board size: {size} x {size}</p>
+
+      <div className='board'>
+
+        <Card image="😺" />
+        <Card image="🐶" />
+        <Card image="🐸" />
+        <Card image="🐻" />
+        
+      </div>
     </div>
   );
 }
