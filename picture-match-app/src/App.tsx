6d81964638b327b46e3a9dee76e2e0145a1e1047
matricarin <matricarin@gmail.com>
2026-08-13 17:11:52 +0500

    game manager was added

--- picture-match-app/src/App.tsx
+++ picture-match-app/src/App.tsx
@@ -5,54 +5,7 @@ import BoardComponent from "./components/BoardComponent";
 function App() {
   const title = "Picture Match!";
   const cards: Card[] = [
-    {
-      id: 1,
-      image: "🐰",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 2,
-      image: "🦁",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 3,
-      image: "🐸",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 4,
-      image: "🐵",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 5,
-      image: "😺",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 6,
-      image: "🐶",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 7,
-      image: "🐮",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 8,
-      image: "🐼",
-      isOpen: false,
-      isMatched: false
-    },
+
   ]
 
   return (
---
+++ picture-match-app/src/Game.tsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import BoardComponent from "./components/BoardComponent";
+import {type Card } from "./models/Card";
+
+const initialCards: Card[] = [
+    {
+      id: 1,
+      image: "🐰",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 2,
+      image: "🦁",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 3,
+      image: "🐸",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 4,
+      image: "🐵",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 5,
+      image: "😺",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 6,
+      image: "🐶",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 7,
+      image: "🐮",
+      isOpen: false,
+      isMatched: false
+    },
+    {
+      id: 8,
+      image: "🐼",
+      isOpen: false,
+      isMatched: false
+    },
+];
+
+function Game(){
+    const [cards, setCards] = useState<Card[]>(initialCards);
+    return(
+        <BoardComponent cards={cards}/>
+    );
+};
+
+export default Game;
