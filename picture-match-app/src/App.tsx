ee1cc5187efe4de8d9509ecb18fdf21bf136f64f
matricarin <matricarin@gmail.com>
2026-08-13 16:12:47 +0500

    new cards were added

--- picture-match-app/src/App.tsx
+++ picture-match-app/src/App.tsx
@@ -6,27 +6,43 @@ function App() {
   const title = "Picture Match!";
   const cards = [
     {
-      id:1,
-      image:"🐰"
+      id: 1,
+      image: "🐰"
     },
     {
-      id:2,
-      image:"🦁"
+      id: 2,
+      image: "🦁"
     },
     {
-      id:3,
-      image:"🐸"
+      id: 3,
+      image: "🐸"
     },
     {
-      id:4,
-      image:"🐵"
+      id: 4,
+      image: "🐵"
+    },
+    {
+      id: 5,
+      image: "😺"
+    },
+    {
+      id: 6,
+      image: "🐶"
+    },
+    {
+      id: 7,
+      image: "🐮"
+    },
+    {
+      id: 8,
+      image: "🐼"
     },
   ]
 
   return (
     <div>
       <h1>{title}</h1>
-      <Board cards={cards}/>
+      <Board cards={cards} />
     </div>
   );
 }
