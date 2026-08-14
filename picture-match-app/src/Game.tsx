f663b7f4e677c476b0ca921195ab2eaedd028080
matricarin <matricarin@gmail.com>
2026-08-14 05:57:52 +0500

    handler for event was added

--- picture-match-app/src/Game.tsx
+++ picture-match-app/src/Game.tsx
@@ -1,63 +1,69 @@
-import {useState} from "react";
+import { useState } from "react";
 import BoardComponent from "./components/BoardComponent";
-import {type Card } from "./models/Card";
+import { type Card } from "./models/Card";
 
 const initialCards: Card[] = [
-    {
-      id: 1,
-      image: "🐰",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 2,
-      image: "🦁",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 3,
-      image: "🐸",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 4,
-      image: "🐵",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 5,
-      image: "😺",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 6,
-      image: "🐶",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 7,
-      image: "🐮",
-      isOpen: false,
-      isMatched: false
-    },
-    {
-      id: 8,
-      image: "🐼",
-      isOpen: false,
-      isMatched: false
-    },
+  {
+    id: 1,
+    image: "🐰",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 2,
+    image: "🦁",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 3,
+    image: "🐸",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 4,
+    image: "🐵",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 5,
+    image: "😺",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 6,
+    image: "🐶",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 7,
+    image: "🐮",
+    isOpen: false,
+    isMatched: false
+  },
+  {
+    id: 8,
+    image: "🐼",
+    isOpen: false,
+    isMatched: false
+  },
 ];
 
-function Game(){
-    const [cards, setCards] = useState<Card[]>(initialCards);
-    return(
-        <BoardComponent cards={cards}/>
-    );
+function Game() {
+  const [cards, setCards] = useState<Card[]>(initialCards);
+
+  function handleCardClick(cardId: number) {
+    console.log("Clicked card:", cardId);
+  };
+
+  return (
+    <BoardComponent cards={cards}
+      onCardClick={handleCardClick} />
+  );
 };
 
 export default Game;
