870f5ff84a5d79a85f10a95d8e7f0ee248ac056a
matricarin <matricarin@gmail.com>
2026-08-14 07:16:09 +0500

    deck creation was invoked

--- picture-match-app/src/Game.tsx
+++ picture-match-app/src/Game.tsx
@@ -1,60 +1,10 @@
 import { useState, useEffect } from "react";
 import BoardComponent from "./components/BoardComponent";
 import { type Card } from "./models/Card";
-
-const initialCards: Card[] = [
-  {
-    id: 1,
-    image: "🐰",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 2,
-    image: "🦁",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 3,
-    image: "🐸",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 4,
-    image: "🐵",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 5,
-    image: "😺",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 6,
-    image: "🐶",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 7,
-    image: "🐮",
-    isOpen: false,
-    isMatched: false
-  },
-  {
-    id: 8,
-    image: "🐼",
-    isOpen: false,
-    isMatched: false
-  },
-];
+import { createDeck } from "./game/Deck";
 
 function Game() {
-  const [cards, setCards] = useState<Card[]>(initialCards);
+  const [cards, setCards] = useState<Card[]>(() => createDeck());
 
   const [selectedCardsIds, setSelectedCardsIds] = useState<number[]>([]);
 
